bb45e78e0a9dc17fdd7323f0278aedc743707fef
Rodolfo Yabut <rodolfoyabut@gmail.com>
2018-04-24 04:46:55 -0700

    Hydrate all the time after all
    Warnings caused by whitespace in initial HTML

--- src/client/index.js
+++ src/client/index.js
@@ -3,9 +3,7 @@ import { hydrate, render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from '../client/App'
 
-const renderMethod = process.env.NODE_ENV === 'development' ? render : hydrate
-
-renderMethod(
+hydrate(
   <BrowserRouter>
     <App data={window.__INITIAL_DATA__} />
   </BrowserRouter>,

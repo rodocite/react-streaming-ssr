76873ade4bd00de9946ffa260d247359abf1a444
Rodolfo Yabut <rodolfoyabut@gmail.com>
2018-04-24 06:01:30 -0700

    Basic CSS resets

--- src/client/App.js
+++ src/client/App.js
@@ -5,6 +5,14 @@ import { Route, Switch } from 'react-router-dom'
 import Nav from './Nav'
 
 const Container = styled.div`
+  *, *:before, *:after {
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+    font-weight: 100;
+    line-height: 1.5;
+    margin: 0;
+    padding: 0;
+  }
 `
 
 class App extends React.Component {
